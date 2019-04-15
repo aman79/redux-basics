103afea6298bb84e718bc6e4df3b986fef693630
Aman Gupta <gupta-dxc@hpe.com>
2019-04-15 16:51:08 +0530

    added actionCreator

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -6,3 +6,43 @@ export const STORE_RESULT = 'STORE_RESULT';
 export const DELETE_RESULT = 'DELETE_RESULT';
 
 //action creators is a fun which returns action.
+
+export const increment = () => {
+  return {
+    type: INCREMENT
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT
+  };
+};
+
+export const add = value => {
+  return {
+    type: ADD,
+    val: value
+  };
+};
+
+export const subtract = value => {
+  return {
+    type: SUBTRACT,
+    val: value
+  };
+};
+
+export const storeResult = res => {
+  return {
+    type: STORE_RESULT,
+    result: res
+  };
+};
+
+export const deleteResult = resElId => {
+  return {
+    type: DELETE_RESULT,
+    resultElId: resElId
+  };
+};
